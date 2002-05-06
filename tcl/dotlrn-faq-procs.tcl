2c9cedbe6d2b66b673112905eb27f47757d2d571
Yonatan Feldman <yon@milliped.com>
2002-05-06 23:56:27 +0000

    removed everything pertaining to user's portals for a community, we don't support that anymore

--- tcl/dotlrn-faq-procs.tcl
+++ tcl/dotlrn-faq-procs.tcl
@@ -14,16 +14,6 @@
 #  details.
 #
 
-
-
-#
-# Procs for DOTLRN Faq Applet
-# Copyright 2001 OpenForce, inc.
-# Distributed under the GNU GPL v2
-#
-# October 5th, 2001
-#
-
 ad_library {
 
     Procs to set up the dotLRN Faq applet
@@ -72,15 +62,13 @@ namespace eval dotlrn_faq {
     } {
         Add the faq applet to a specifc community
     } {
-        # portal template stuff
-        # get the portal_template_id by callback
-        set pt_id [dotlrn_community::get_portal_template_id $community_id]
+        set portal_id [dotlrn_community::get_portal_id -community_id $community_id]
 
         # set up the DS for the portal template
-        faq_portlet::make_self_available $pt_id
+        faq_portlet::make_self_available $portal_id
 
         if {[dotlrn_community::dummy_comm_p -community_id $community_id]} {
-            faq_portlet::add_self_to_page $pt_id 0
+            faq_portlet::add_self_to_page $portal_id 0
             return
         }
 
@@ -88,15 +76,12 @@ namespace eval dotlrn_faq {
         set package_key [package_key]
         set package_id [dotlrn::instantiate_and_mount $community_id $package_key]
 
-        faq_portlet::add_self_to_page $pt_id $package_id
+        faq_portlet::add_self_to_page $portal_id $package_id
 
         # set up the DS for the admin page
-        set admin_portal_id [dotlrn_community::get_community_admin_portal_id $community_id]
+        set admin_portal_id [dotlrn_community::get_admin_portal_id -community_id $community_id]
         faq_admin_portlet::make_self_available $admin_portal_id
         faq_admin_portlet::add_self_to_page $admin_portal_id $package_id
-
-        # Set up some permissions
-        # for FAQ, it's all good as is
 
         # return the package_id
         return $package_id
@@ -108,12 +93,6 @@ namespace eval dotlrn_faq {
     } {
         remove the applet from the community
     } {
-        # Remove all instances of the faq portlet! (this is some serious stuff!)
-
-        # Dropping all messages, forums
-
-        # Killing the package
-
     }
 
     ad_proc -public add_user {
@@ -124,42 +103,22 @@ namespace eval dotlrn_faq {
         return
     }
 
+    ad_proc -public remove_user {
+        user_id
+    } {
+    } {
+    }
+
     ad_proc -public add_user_to_community {
         community_id
         user_id
     } {
         Called when a user is added to a specific dotlrn community
     } {
-        # Get the faq applet's package_id by callback
-        set package_id [dotlrn_community::get_applet_package_id \
-                $community_id \
-                dotlrn_faq
-        ]
+        set package_id [dotlrn_community::get_applet_package_id $community_id [applet_key]]
+        set portal_id [dotlrn::get_workspace_portal_id $user_id]
 
-        # Get the portal_id by callback
-        set portal_id [dotlrn_community::get_portal_id $community_id $user_id]
-
-	if { [exists_and_not_null $portal_id] } {
-            # Make faq DS available to this page
-            faq_portlet::make_self_available $portal_id
-            
-            # Call the portal element to be added correctly
-            faq_portlet::add_self_to_page $portal_id $package_id
-        }
-
-        # Now for the user workspace
-        set workspace_portal_id [dotlrn::get_workspace_portal_id $user_id]
-
-        # Add the portlet here
-	if { [exists_and_not_null $workspace_portal_id] } {
-            faq_portlet::add_self_to_page $workspace_portal_id $package_id
-        }
-    }
-
-    ad_proc -public remove_user {
-        user_id
-    } {
-    } {
+        faq_portlet::add_self_to_page $portal_id $package_id
     }
 
     ad_proc -public remove_user_from_community {
@@ -168,25 +127,10 @@ namespace eval dotlrn_faq {
     } {
         Remove a user from a community
     } {
-        # Get the portal_id
-        set portal_id [dotlrn_community::get_portal_id $community_id $user_id]
-
-        # Get the package_id by callback
         set package_id [dotlrn_community::get_applet_package_id $community_id [applet_key]]
+        set portal_id [dotlrn::get_workspace_portal_id $user_id]
 
-        # Remove the portal element
         faq_portlet::remove_self_from_page $portal_id $package_id
-
-        # Buh Bye.
-        faq_portlet::make_self_unavailable $portal_id
-
-        # Remove from main workspace
-        set workspace_portal_id [dotlrn::get_workspace_portal_id $user_id]
-
-        # Remove the portlet
-        if {![empty_string_p $workspace_portal_id]} {
-            faq_portlet::remove_self_from_page $workspace_portal_id $package_id
-        }
     }
 
 }
